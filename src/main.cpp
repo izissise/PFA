6ac79215415ab93d293c6478ef181b78d639e403
Hugues Morisset <morisset.hugues@gmail.com>
2014-07-24 02:33:35 +0200

    Make it compile on multiple platform

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 
-#define UNUSED __attribute__((unused))
+#if defined(__GNUC__)
+# define UNUSED __attribute__((unused))
+#elif defined(WIN32)
+# define UNUSED __pragma(warning(suppress:4100))
+#elif defined(WIN64)
+# define UNUSED __pragma(warning(suppress:4100))
+#elif defined(__APPLE__)
+# define UNUSED __attribute__((unused))
+#endif
 
 int main(UNUSED int ac, UNUSED char *av[])
 {
