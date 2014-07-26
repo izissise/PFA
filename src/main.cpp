8fd195b7d5619052d5b50c0c534d8ec30a3c0eb1
Hugues Morisset <morisset.hugues@gmail.com>
2014-07-26 14:38:49 +0200

    Add include dir

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,7 @@
 #include <iostream>
 
-#if defined(__GNUC__)
-# define UNUSED __attribute__((unused))
-#elif defined(WIN32)
-# define UNUSED __pragma(warning(suppress:4100))
-#elif defined(WIN64)
-# define UNUSED __pragma(warning(suppress:4100))
-#elif defined(__APPLE__)
-# define UNUSED __attribute__((unused))
-#endif
+#include "Unused.hpp"
+
 
 int main(UNUSED int ac, UNUSED char *av[])
 {
