cfe0a06c87b3c58c46adbd24732535d4563f3892
Hugues Morisset <morisset.hugues@gmail.com>
2014-07-28 18:06:01 +0200

    Change path for source adn test in runTest script

--- scripts/runTest.sh
+++ scripts/runTest.sh
@@ -9,8 +9,8 @@ cd Debug
 cmake -DCMAKE_BUILD_TYPE=Debug -DCOVERAGE=True ..
 make
 cd ..
-lcov -d Debug/CMakeFiles/${PROJECT}.dir/src/main/cpp -d Debug/CMakeFiles/UnitTester.dir/src/test/cpp --zerocounters
+lcov -d Debug/CMakeFiles/${PROJECT}.dir/src/ -d Debug/CMakeFiles/UnitTester.dir/test/ --zerocounters
 Debug/UnitTester
-lcov -d Debug/CMakeFiles/${PROJECT}.dir/src/main/cpp -d Debug/CMakeFiles/UnitTester.dir/src/test/cpp --capture --output-file coverage/${PROJECT}.info
+lcov -d Debug/CMakeFiles/${PROJECT}.dir/src/ -d Debug/CMakeFiles/UnitTester.dir/test/ --capture --output-file coverage/${PROJECT}.info
 lcov -r coverage/${PROJECT}.info "/usr/include/*" -o coverage/${PROJECT}.info
 genhtml -o coverage coverage/${PROJECT}.info
