2f9f9a7a1ba4d6da9971c8fdb113ebc830d77b66
Tastyep <luc@localhost.localdomain>
2014-08-20 14:07:27 +0200

    Started the command line parser Get the content of the file, tokenize it and call the right pasing function

--- pfaexe/src/Settings.cpp
+++ pfaexe/src/Settings.cpp
@@ -1,7 +1,14 @@
+#include <fstream>
+#include <sstream>
+#include <iterator>
 #include "Settings.hpp"
+#include "Exception.hpp"
 
 Settings::Settings()
 {
+  _parseKey["set"] = &Settings::setKeyword;
+  _parseKey["bind"] = &Settings::bindKeyword;
+  loadConfigFile("../config.cfg");
 }
 
 Settings::~Settings()
@@ -11,4 +18,46 @@ Settings::~Settings()
 CvarList	&Settings::getCvarList()
 {
   return (_vars);
+}
+
+void	Settings::setKeyword(const std::vector<std::string> &tokens)
+{
+  std::cout << "set Keyword function" << std::endl;
+}
+
+void	Settings::bindKeyword(const std::vector<std::string> &tokens)
+{
+  std::cout << "bind Keyword function" << std::endl;
+}
+
+void	Settings::parseCommandLine(const std::string &cmd)
+{
+  std::istringstream			buf(cmd);
+  std::istream_iterator<std::string>	beg(buf), end;
+  std::vector<std::string>		tokens(beg, end);
+
+  if (tokens.empty())
+    return ;
+  auto elem = _parseKey.find(*tokens.begin());
+  if (elem == _parseKey.end())
+    throw (Exception("Invalid Keyword ["+ *tokens.begin() + "]"));
+  (this->*(elem->second))(tokens);
+}
+
+void	Settings::loadConfigFile(const std::string &filename)
+{
+  std::ifstream	file(filename.c_str());
+  std::string	line;
+  std::vector<std::string>	content;
+
+  if (file.is_open())
+    {
+      while (std::getline(file, line))
+	content.push_back(line);
+      file.close();
+    }
+  else
+    throw (Exception("File [" + filename + "] not found"));
+  for (const auto &it : content)
+    parseCommandLine(it);
 }
--- pfaexe/src/Settings.hpp
+++ pfaexe/src/Settings.hpp
@@ -1,6 +1,9 @@
 #ifndef _SETTINGS_H_
 #define _SETTINGS_H_
 
+# include <map>
+# include <vector>
+# include <string>
 # include "Cvar.hpp"
 
 class Settings
@@ -10,8 +13,16 @@ public:
   virtual ~Settings();
 
   CvarList	&getCvarList();
+  void		loadConfigFile(const std::string &filename);
+  void		parseCommandLine(const std::string &cmd);
+
 private:
-  CvarList	_vars;
+  typedef	void (Settings::*func)(const std::vector<std::string> &vec);
+  void		setKeyword(const std::vector<std::string> &tokens);
+  void		bindKeyword(const std::vector<std::string> &tokens);
+
+  CvarList			_vars;
+  std::map<std::string, func>	_parseKey;
 };
 
 #endif /* _SETTINGS_H_ */
