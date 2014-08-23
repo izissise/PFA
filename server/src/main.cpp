de8c63ff3caa97d5583bfa9806620cb466388974
Adrien Della Maggiora <adrien.dellamaggiora@epitech.eu>
2014-08-23 21:20:51 +0200

    Add the server folder and rename pfaexe to client

---
+++ server/src/main.cpp
@@ -0,0 +1,24 @@
+/**
+ * \file main.c
+ * \brief Main binary.
+ * \date 12 august 2014
+ *
+ * It is the source of the main binary of client or server (one will be needed for both),
+ * The infinite loop of the game should be executed here and not in the lib (for testing purpose),
+ * Also it would be good that a try catch block do some advanced error reporting.
+ *
+ */
+
+#include <iostream>
+
+#include "Config.h"
+#include "Unused.hpp"
+#include "printv.hpp"
+
+int main(UNUSED int ac, UNUSED char *av[])
+{
+    printv(std::cout, "Program version: % %\n", xstr(PROJECT_VERSION), xstr(CURRENT_DATE));
+    testBoost();
+    testSfml();
+    return 0;
+}
