fb79511764f94ec2f46b43b5dd62aabf15ebd6a2
Hugues Morisset <morisset.hugues@gmail.com>
2014-08-24 13:46:56 +0200

    Add include file into globbing and cleanup a little

---
+++ cmake/Modules/DemangleCppABI.cmake
@@ -0,0 +1,33 @@
+#
+#
+# Copyright (C) 2011 by Johannes Wienke <jwienke at techfak dot uni-bielefeld dot de>
+#
+# This file may be licensed under the terms of the
+# GNU Lesser General Public License Version 3 (the ``LGPL''),
+# or (at your option) any later version.
+#
+# Software distributed under the License is distributed
+# on an ``AS IS'' basis, WITHOUT WARRANTY OF ANY KIND, either
+# express or implied. See the LGPL for the specific language
+# governing rights and limitations.
+#
+# You should have received a copy of the LGPL along with this
+# program. If not, go to http://www.gnu.org/licenses/lgpl.html
+# or write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+#
+# The development of this software was supported by:
+#   CoR-Lab, Research Institute for Cognition and Robotics
+#     Bielefeld University
+
+INCLUDE(CheckIncludeFileCXX)
+
+# decide how to do name demangling
+CHECK_INCLUDE_FILE_CXX("cxxabi.h" HAVE_CXXABI_H)
+IF(HAVE_CXXABI_H)
+  ADD_DEFINITIONS(-DDEMANGLE_GCC)
+ELSEIF(MSVC)
+  ADD_DEFINITIONS(-DDEMANGLE_MSVC)
+ELSE()
+  MESSAGE(WARNING "No demangling solution found for the system.")
+ENDIF()
--- pfa/src/printv.cpp
+++ pfa/src/printv.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <boost/asio.hpp>
 #include <boost/array.hpp>
+
 #include "printv.hpp"
 
 std::ostream& printv(std::ostream& f, const std::string& s)
@@ -34,16 +35,16 @@ void testBoost()
 {
 	// Création du service principal et du résolveur.
 	boost::asio::io_service ios;
-    
+
 	// On veut se connecter sur la machine locale, port 7171
 	tcp::endpoint endpoint(boost::asio::ip::address::from_string("127.0.0.1"), 7171);
-    
+
 	// On crée une socket // (1)
 	tcp::socket socket(ios);
-    
+
 	// Tentative de connexion, bloquante // (2)
 	socket.connect(endpoint);
-    
+
 	// Création du buffer de réception // (3)
 	boost::array<char, 128> buf;
 	while (1)
@@ -51,13 +52,13 @@ void testBoost()
 		boost::system::error_code error;
 		// Réception des données, len = nombre d'octets reçus // (4)
 		int len = socket.read_some(boost::asio::buffer(buf), error);
-        
+
 		if (error == boost::asio::error::eof) // (5)
 		{
 			std::cout << "\nTerminé !" << std::endl;
 			break;
 		}
-        
+
 		// On affiche (6)
 		std::cout.write(buf.data(), len);
 	}
