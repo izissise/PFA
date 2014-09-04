b1411dfa56769821626e7602fa65f2f7e3e87ec6
Tastyep <luc.sinet@epitech.eu>
2014-09-04 11:45:48 +0200

    Implemented Console's history
    Made correct padding to not look ugly
    Can set the history size on constructor

--- client/src/Console.cpp
+++ client/src/Console.cpp
@@ -1,12 +1,13 @@
 #include "Console.hpp"
 #include "Exception.hpp"
 
-Console::Console(Settings * const set) : _set(set)
+Console::Console(Settings * const set) : _set(set), _history()
 {
   sf::Color	color(20, 20, 20);
+  int		width = std::stoi(set->getCvarList().getCvar("r_width"));
+  int		height = std::stoi(set->getCvarList().getCvar("r_height"));
 
-  _rectangle.setSize(sf::Vector2f(std::stoi(set->getCvarList().getCvar("r_width")),
-				  std::stoi(set->getCvarList().getCvar("r_height")) / 2));
+  _rectangle.setSize(sf::Vector2f(width, height / 2));
   _rectangle.setPosition(0, 0);
   _rectangle.setOutlineColor(sf::Color::White);
   _rectangle.setOutlineThickness(2);
@@ -17,7 +18,7 @@ Console::Console(Settings * const set) : _set(set)
   _text.setFont(_font);
   _text.setStyle(sf::Text::Regular);
   _text.setColor(sf::Color::White);
-  _text.setCharacterSize(20);
+  _text.setCharacterSize(FONTSIZE);
 }
 
 Console::~Console()
@@ -29,7 +30,6 @@ void		Console::run(sf::RenderWindow &window, sf::Event event)
 {
   Controls	&ctrl = _set->getControls();
 
-  _text.setString(_input.getString());
   if (_input.getInput(event))
     {
       try {
@@ -38,6 +38,9 @@ void		Console::run(sf::RenderWindow &window, sf::Event event)
       catch (const Exception &e) {
 	std::cerr << e.what() << std::endl;
       }
+      _history.content.push_front(_input.getString());
+      if (_history.content.size() > _history.maxSize)
+	_history.content.pop_back();
       _input.clear();
     }
   if (event.type == sf::Event::KeyPressed)
@@ -46,8 +49,23 @@ void		Console::run(sf::RenderWindow &window, sf::Event event)
     ctrl.releaseKey(event.key.code);
 }
 
-void		Console::draw(sf::RenderWindow &window) const
+void		Console::draw(sf::RenderWindow &window)
 {
+  unsigned int	wSize = (FONTSIZE + 2 * LINESPACING);
+  unsigned int	cSize;
+
+  cSize = std::stoi(_set->getCvarList().getCvar("r_height")) / 2 - wSize;
   window.draw(_rectangle);
+  _text.setPosition(0, cSize - (FONTSIZE + 3 * LINESPACING));
+  for (auto it = _history.content.cbegin() + _history.pos;
+       it != _history.content.cend() && wSize < cSize; ++it)
+    {
+      _text.setString(*it);
+      window.draw(_text);
+      _text.move(0, -(FONTSIZE + LINESPACING));
+      wSize += (FONTSIZE + LINESPACING);
+    }
+  _text.setPosition(0, cSize);
+  _text.setString("]" + _input.getString());
   window.draw(_text);
 }
--- client/src/Console.hpp
+++ client/src/Console.hpp
@@ -2,8 +2,23 @@
 #define _CONSOLE_H_
 
 #include <SFML/Graphics.hpp>
+#include <deque>
 #include "Settings.hpp"
 #include "UserInput.hpp"
+
+#define FONTSIZE 20
+#define LINESPACING 4
+
+typedef struct			s_history
+{
+  std::deque<sf::String>	content;
+  unsigned int			maxSize;
+  unsigned int			pos;
+
+  s_history(unsigned int s = 100) : maxSize(s), pos(0)
+  {
+  }
+}				t_history;
 
 class Console
 {
@@ -12,11 +27,12 @@ public:
   virtual ~Console();
 
   void		run(sf::RenderWindow &window, sf::Event event);
-  void		draw(sf::RenderWindow &window) const;
+  void		draw(sf::RenderWindow &window);
 
 private:
   Settings		*_set;
   UserInput		_input;
+  t_history		_history;
   sf::RectangleShape	_rectangle;
   sf::Font		_font;
   sf::Text		_text;
