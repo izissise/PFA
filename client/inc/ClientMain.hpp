8670056a10a92285feb8ff3114d52c0ed554792e
Hugues Morisset <morisset.hugues@gmail.com>
2014-09-05 13:30:37 +0200

    Add class for main loop

---
+++ client/inc/ClientMain.hpp
@@ -0,0 +1,16 @@
+#ifndef CLIENTMAIN_H
+#define CLIENTMAIN_H
+
+
+class ClientMain
+{
+	public:
+		/** Default constructor */
+		ClientMain();
+		/** Default destructor */
+		virtual ~ClientMain();
+	protected:
+	private:
+};
+
+#endif // CLIENTMAIN_H
---
+++ client/src/ClientMain.cpp
@@ -0,0 +1,11 @@
+#include "ClientMain.hpp"
+
+ClientMain::ClientMain()
+{
+	//ctor
+}
+
+ClientMain::~ClientMain()
+{
+	//dtor
+}
