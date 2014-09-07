5013518d85befa89c2ae013f06c3aad4518692fd
Tastyep <luc.sinet@epitech.eu>
2014-09-07 13:15:34 +0200

    add .hpp files

---
+++ client/inc/APanelScreen.hpp
@@ -0,0 +1,28 @@
+#ifndef _APANELSCREEN_H_
+#define _APANELSCREEN_H_
+
+#include <vector>
+#include "Widget.hpp"
+
+class APanelScreen
+{
+public:
+  APanelScreen(sf::Texture * const texture);
+  virtual ~APanelScreen() = 0;
+
+  virtual int	run(const sf::Event &event, sf::RenderWindow &ref, Settings &set) = 0;
+  virtual void	draw(sf::RenderWindow &window);
+
+protected:
+  void		saveTexture(sf::Texture * const texture);
+  sf::Texture	*addSpriteForWidget(AWidget * const widget,
+				    const sf::Color &color,
+				    const sf::Vector2u &size);
+
+protected:
+  sf::Font			_font;
+  std::vector<sf::Texture *>	_textures;
+  std::vector<AWidget *>	_widgets;
+};
+
+#endif /* _APANELSCREEN_H_ */
