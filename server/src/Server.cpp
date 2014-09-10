1c94351dba5b67a21c009201b82d68f8403d822a
Adrien Della Maggiora <adrien.dellamaggiora@epitech.eu>
2014-09-10 17:25:22 +0200

    add a simple netcat server

--- server/src/Server.cpp
+++ server/src/Server.cpp
@@ -1,15 +1,14 @@
 #include "server.hpp"
 
 Server::Server(t_arg &arg)
-: _arg(arg)
+: _arg(arg), _clients()
 {
     if (enet_initialize() != 0)
         throw (NetworkException("An error occurred while initializing ENet."));
 
-//    enet_address_set_host (&_address, "10.10.253.245");
     _address.host = ENET_HOST_ANY;
     _address.port = _arg.port;
-    _server = enet_host_create(&_address, 128, 1, 10, 10);
+    _server = enet_host_create(&_address, 128, 2, 10, 10);
 
     if (_server == NULL)
         throw (NetworkException("An error occurred while trying to create an ENet server host."));
@@ -29,26 +28,27 @@ void Server::run()
     std::cout << "Quiet => " << _arg.quiet << std::endl;
     std::cout << "Debug => " << _arg.debug << std::endl;
 
-    int val;
-
-    while ((val = enet_host_service(_server, &event, 1000)) >= 0)
+    while ((enet_host_service(_server, &event, 1000)) >= 0)
     {
-        std::cout << "Val => " << val << std::endl;
         switch (event.type)
         {
             case ENET_EVENT_TYPE_CONNECT:
                 std::cout << "A new client connected from " << event.peer->address.host << ":" << event.peer->address.port << std::endl;
-                event.peer->data = (char *)"Client information";
+                _clients.push_back(new Client(event.peer));
+                event.peer->data = (char *)"Client";
                 break;
             case ENET_EVENT_TYPE_RECEIVE:
-                std::cout << "A packet of length " << event.packet->dataLength << "containing " << event.packet->data
-                          << " was received from " << event.peer->data << " on channel " << event.channelID << std::endl;
+                for (auto it = _clients.begin();it != _clients.end();it++)
+                    (*it)->sendPacket(0, std::string(reinterpret_cast<char *>(event.packet->data)));
+               std::cout << "A packet of length " << event.packet->dataLength << " containing [" << event.packet->data
+                          << "] was received from " << event.peer->data << " on channel " << (int)event.channelID << std::endl;
                 enet_packet_destroy (event.packet);
                 break;
             case ENET_EVENT_TYPE_DISCONNECT:
-                std::cout << event.peer->data << " disconnected." << std::endl;
+                std::cout << "Client disconnected" << std::endl;
+                _clients.erase(std::find_if(_clients.begin(), _clients.end(),
+                                           [event] (const Client *elem) { return elem->getPeer() == event.peer;}));
                 event.peer->data = NULL;
-			case ENET_EVENT_TYPE_NONE:
             default:
                 break;
         }
--- server/src/client.cpp
+++ server/src/client.cpp
@@ -1,6 +1,27 @@
 #include "client.hpp"
 
-Client::Client()
+Client::Client(ENetPeer *peer)
+: _peer(peer)
 {
-    
+    std::cout << peer << std::endl;
+    std::cout << _peer << std::endl;
+}
+
+Client::~Client()
+{
+
+}
+
+void Client::sendPacket(enet_uint8 chan, const std::string &packet) const
+{
+    ENetPacket *message = enet_packet_create (packet.c_str(),
+                                              packet.size() + 1,
+                                              ENET_PACKET_FLAG_RELIABLE);
+    if (message == NULL || enet_peer_send(_peer, chan, message) != 0)
+        throw (NetworkException("Cannot send the message"));
+}
+
+const ENetPeer *Client::getPeer() const
+{
+    return (_peer);
 }
