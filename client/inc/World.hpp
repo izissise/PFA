f695baf07f5cf8865865d83bce1a61c32dfb05f8
Christopher Steel <steel_c@epitech.eu>
2014-09-23 15:53:09 +0200

    Camera class created inside World for increased clarity

--- client/inc/World.hpp
+++ client/inc/World.hpp
@@ -14,33 +14,60 @@ class World
 {
 public:
   World(Settings& set);
-  virtual ~World(void) = default;
+  ~World(void) = default;
   World(const World& other) = delete;
   World&		operator=(const World& other) = delete;
 
   void			draw(sf::RenderWindow& window) const;
 
-  void			translateCamera(const sf::Vector2<float>& v);
-  void			moveCamera(const sf::Vector2<float>& pos);
-
 protected:
 private:
   typedef sf::Vector2f worldPos;
   typedef sf::Vector2i screenPos;
+  typedef std::pair<int, int> chunkId;
+
+  class Camera
+  {
+  public:
+    Camera(void) = default;
+    ~Camera(void) = default;
+    Camera(const Camera& other) = default;
+    Camera&	operator=(const Camera& other) = default;
+
+    void	translate(const sf::Vector2<float>& v) noexcept;
+    void	move(const sf::Vector2<float>& pos) noexcept;
+
+    void	scale(const sf::Vector2<float>& s) noexcept;
+    void	resize(const sf::Vector2<float>& s) noexcept;
+
+    float	left(void) const noexcept	{ return _topLeft.x; }
+    float	right(void) const noexcept	{ return _topLeft.x + _size.x; }
+    float	top(void) const noexcept	{ return _topLeft.y; }
+    float	bottom(void) const noexcept	{ return _topLeft.y + _size.y; }
+    float	width(void) const noexcept	{ return _size.x; }
+    float	height(void) const noexcept	{ return _size.y; }
+    worldPos	center(void) const noexcept	{ return _center; }
+
+  private:
+    worldPos	_topLeft;
+    worldPos	_size;
+    worldPos	_center;
+  };
 
   worldPos		_sToWPos(screenPos pos) const;
   screenPos		_wToSPos(worldPos pos) const;
+  float			_getGridOffset(float w) const;
 
   void			_drawChunk(sf::RenderWindow& window,
-				   const std::pair<int, int>& chunkCursor,
+				   const chunkId& chunkCursor,
 				   sf::Vector2<int>& windowCoord) const;
+public:
+  Camera			camera;
 
-  std::map<std::pair<int, int>, Chunk *>	_chunks;
+private:
+  std::map<chunkId, Chunk *>	_chunks;
   Settings&			_set;
   screenPos			_screenSize;
-  worldPos			_camPos;
-  worldPos			_camSize;
-  worldPos			_camCenter;
   TileCodex			_codex;
 };
 
--- client/src/World.cpp
+++ client/src/World.cpp
@@ -5,18 +5,17 @@
 
 World::World(Settings& set) :
   _set(set),
-  _camCenter(0.5f, 0.5f)
+  _screenSize(std::stoi(set.getCvarList().getCvar("r_width")),
+	      std::stoi(set.getCvarList().getCvar("r_height")))
 {
-  _screenSize = {std::stoi(set.getCvarList().getCvar("r_width")),
-		 std::stoi(set.getCvarList().getCvar("r_height"))};
-  _camSize = _sToWPos(_screenSize);
-  _camPos = _camCenter - _camSize / 2.0f;
+  chunkId	first;
+  chunkId	last;
 
-  std::pair<int, int>	first;
-  std::pair<int, int>	last;
+  camera.resize(_sToWPos(_screenSize));
+  camera.move({0.5f, 0.5f});
 
-  first = {floor(_camPos.x), floor(_camPos.y)};
-  last = {floor(_camPos.x + _camSize.x), floor(_camPos.y + _camSize.y)};
+  first = {floor(camera.left()), floor(camera.top())};
+  last = {floor(camera.right()), floor(camera.bottom())};
   for (auto cursor = first; cursor.second <= last.second; ++cursor.second) {
     for (cursor.first = first.first; cursor.first <= last.first; ++cursor.first) {
       _chunks[cursor] = new Chunk();
@@ -27,21 +26,18 @@ World::World(Settings& set) :
 
 void World::draw(sf::RenderWindow& window) const
 {
-  std::pair<int, int>	first;
-  std::pair<int, int>	last;
-  worldPos		worldOrigin;
-  screenPos		screenOrigin;
-  screenPos		screenCoord;
-  auto			getNormOffset = [](float w){
-    return -(w - floor(w));
-  };
+  chunkId	first;
+  chunkId	last;
+  worldPos	worldOrigin;
+  screenPos	screenOrigin;
+  screenPos	screenCoord;
 
-  worldOrigin.x = getNormOffset(_camPos.x);
-  worldOrigin.y = getNormOffset(_camPos.y);
+  worldOrigin.x = _getGridOffset(camera.left());
+  worldOrigin.y = _getGridOffset(camera.top());
   screenOrigin = _wToSPos(worldOrigin);
   screenCoord = screenOrigin;
-  first = {floor(_camPos.x), floor(_camPos.y)};
-  last = {floor(_camPos.x + _camSize.x), floor(_camPos.y + _camSize.y)};
+  first = {floor(camera.left()), floor(camera.top())};
+  last = {floor(camera.right()), floor(camera.bottom())};
   for (auto cursor = first; cursor.second <= last.second; ++cursor.second) {
     for (cursor.first = first.first; cursor.first <= last.first; ++cursor.first) {
       _drawChunk(window, cursor, screenCoord);
@@ -50,20 +46,6 @@ void World::draw(sf::RenderWindow& window) const
     screenCoord.x = screenOrigin.x;
     screenCoord.y += Chunk::height * TileCodex::tileSize;
   }
-}
-
-void World::translateCamera(const sf::Vector2<float>& v)
-{
-  _camCenter += v;
-  _camPos.x += v.x;
-  _camPos.y += v.y;
-}
-
-void World::moveCamera(const sf::Vector2<float>& pos)
-{
-  _camCenter = pos;
-  _camPos.x = _camCenter.x - _camSize.x / 2.0f;
-  _camPos.y = _camCenter.y - _camSize.y / 2.0f;
 }
 
 auto World::_sToWPos(screenPos pos) const -> worldPos
@@ -78,8 +60,13 @@ auto World::_wToSPos(worldPos pos) const -> screenPos
       static_cast<int>(pos.y * (Chunk::height * TileCodex::tileSize))};
 }
 
+float World::_getGridOffset(float w) const
+{
+    return -(w - floor(w));
+}
+
 void World::_drawChunk(sf::RenderWindow& window,
-		       const std::pair<int, int>& chunkCursor,
+		       const chunkId& chunkCursor,
 		       sf::Vector2<int>& windowCoord) const
 {
   Chunk	*chunk;
