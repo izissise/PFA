e9e1f6fe5ecb48320e7dc9736b226e7b5996cda5
Christopher Steel <steel_c@epitech.eu>
2014-12-06 21:00:19 +0100

    Added Entityx as a required library

---
+++ cmake/Modules/FindEntityx.cmake
@@ -0,0 +1,35 @@
+# Try to find entityx library
+# Once done this will define
+#
+# ENTITYX_FOUND - true if system found entityx library
+# ENTITYX_INCLUDE_DIRS - the entityx include directories
+# ENTITYX_LIBRARIES - the libraries needed to use entityx
+#
+# $ENTITYX_ROOT is an environment variable that can be used to find entityx
+#
+
+
+FIND_PATH(ENTITYX_INCLUDE_DIRS	entityx/entityx.h
+				PATHS
+				${ENV}${ENTITYX_ROOT}
+				/usr/local
+				/usr
+				PATH_SUFFIXES include
+				)
+
+FIND_LIBRARY(ENTITYX_LIBRARY	NAMES entityx
+				PATHS
+				${ENV}${ENTITYX_ROOT}
+				/usr/local
+				/usr
+				PATH_SUFFIXES lib
+				)
+
+INCLUDE(FindPackageHandleStandardArgs)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(Entityx DEFAULT_MSG ENTITYX_LIBRARY ENTITYX_INCLUDE_DIRS)
+
+IF(NOT ENTITYX_FOUND)
+  MESSAGE(STATUS "Entityx not found")
+ENDIF()
+
+MARK_AS_ADVANCED(ENTITYX_LIBRARY ENTITYX_INCLUDE_DIRS)
