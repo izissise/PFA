4b94cffc30cb94144ea93af74a48a4d80d5210a6
Tastyep <luc.sinet@epitech.eu>
2014-12-23 15:03:10 +0100

    Renamed it and made it compilable (cmake didnt rebuild the dependencies

--- client/src/ServerInfoPanel.cpp
+++ client/src/ServerInfoPanel.cpp
@@ -1,19 +1,19 @@
-#include "ServerPanel.hpp"
+#include "ServerInfoPanel.hpp"
 
-ServerPanel::ServerPanel(const sf::FloatRect &zone,
+ServerInfoPanel::ServerInfoPanel(const sf::FloatRect &zone,
 			 const std::string &ip) :
-  APanelScreen(zone), _ip(ip);
+  APanelScreen(zone), _ip(ip)
 {
   addFont("default", "../client/assets/default.TTF");
 }
 
-void	ServerPanel::construct(const sf::Texture &texture, Settings &set,
+void	ServerInfoPanel::construct(const sf::Texture &texture, Settings &set,
 			       const std::vector<APanelScreen *> &panels)
 {
   Widget	*wName = new Widget("Name", {_zone.left, _zone.top, 25.f * _zone.width / 100.f, 30},
 				    sf::Text("Server's Name", _font["default"], 20));
   Widget	*wCountry = new Widget("Country", {_zone.left + 25.f * _zone.width / 100.f, _zone.top,
-						5.f * _zone.width / 100.f, 30}
+						5.f * _zone.width / 100.f, 30},
 						sf::Text("NA", _font["default"], 20));
   Widget	*wPlayers = new Widget("Players", {_zone.left + 30.f * _zone.width / 100.f, _zone.top,
 					10.f * _zone.width / 100.f, 30},
@@ -21,20 +21,19 @@ void	ServerPanel::construct(const sf::Texture &texture, Settings &set,
   Widget	*wPing = new Widget("Ping", {_zone.left + 40.f * _zone.width / 100.f, _zone.top,
 					10.f * _zone.width / 100.f, 30},
 					sf::Text("nb ms", _font["default"], 20));
-  unsigned int	id = panels[0].getSubPanels().size();
 
   createNameWidget(wName);
   createCountryWidget(wCountry);
   createPlayerWidget(wPlayers);
   createPingWidget(wPing);
-  applyButtonStyle(wName, id);
-  applyButtonStyle(wCountry, id);
-  applyButtonStyle(wPlayers, id);
-  applyButtonStyle(wPing, id);
-  addWidget{wName, wCountry, wPlayers, wPing};
+  applyButtonStyle(wName);
+  applyButtonStyle(wCountry);
+  applyButtonStyle(wPlayers);
+  applyButtonStyle(wPing);
+  addWidget({wName, wCountry, wPlayers, wPing});
 }
 
-void	ServerPanel::createNameWidget(Widget *widget)
+void	ServerInfoPanel::createNameWidget(Widget *widget)
 {
   std::function	<int (AWidget &widget, const sf::Event &event, sf::RenderWindow &ref)>
     updateFunc;
@@ -44,11 +43,10 @@ void	ServerPanel::createNameWidget(Widget *widget)
     {
       return 0;
     };
-  createButtonStyle(widget, texture);
   widget->setUpdate(updateFunc);
 }
 
-void	ServerPanel::createCountryWidget(Widget *widget)
+void	ServerInfoPanel::createCountryWidget(Widget *widget)
 {
   std::function	<int (AWidget &widget, const sf::Event &event, sf::RenderWindow &ref)>
     updateFunc;
@@ -58,11 +56,10 @@ void	ServerPanel::createCountryWidget(Widget *widget)
     {
       return 0;
     };
-  createButtonStyle(widget, texture);
   widget->setUpdate(updateFunc);
 }
 
-void	ServerPanel::createPlayerWidget(Widget *widget)
+void	ServerInfoPanel::createPlayerWidget(Widget *widget)
 {
   std::function	<int (AWidget &widget, const sf::Event &event, sf::RenderWindow &ref)>
     updateFunc;
@@ -72,11 +69,10 @@ void	ServerPanel::createPlayerWidget(Widget *widget)
     {
       return 0;
     };
-  createButtonStyle(widget, texture);
   widget->setUpdate(updateFunc);
 }
 
-void	ServerPanel::createPingWidget(Widget *widget)
+void	ServerInfoPanel::createPingWidget(Widget *widget)
 {
   std::function	<int (AWidget &widget, const sf::Event &event, sf::RenderWindow &ref)>
     updateFunc;
@@ -86,13 +82,12 @@ void	ServerPanel::createPingWidget(Widget *widget)
     {
       return 0;
     };
-  createButtonStyle(widget, texture);
   widget->setUpdate(updateFunc);
 }
 
-void	ServerPanel::applyButtonStyle(Widget *widget, unsigned int id);
+void	ServerInfoPanel::applyButtonStyle(Widget *widget)
 {
   sf::FloatRect zone = widget->getZone();
 
-  zone->alignText({zone.left, zone.top}, {zone.width, 100}, 50, 50);
+  widget->alignText({zone.left, zone.top}, {zone.width, 100}, 50, 50);
 }
