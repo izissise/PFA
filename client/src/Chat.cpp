ccaca5ae4721a942f05a73803363d3a7e0cca758
Tastyep <luc.sinet@epitech.eu>
2015-02-26 15:01:14 +0100

    can now send message in the game

--- client/src/Chat.cpp
+++ client/src/Chat.cpp
@@ -28,13 +28,23 @@ void	Chat::draw(sf::RenderTarget &window)
   unsigned int		count = 0;
   auto			&fm = FontManager<>::instance();
   Widget		widget("", sf::FloatRect(0,size.y,size.x, 40),
-			       sf::Text("", *fm.get("default.TTF")));
+			       sf::Text("", *fm.get("default.TTF"), 25));
+  const sf::FloatRect	&zone = widget.getZone();
 
-  for (auto &elem : _messages)
+  for (auto it = _messages.rbegin(); it != _messages.rend(); ++it)
     {
-      widget.move(0, -40);
-      const sf::FloatRect &zone = widget.getZone();
+      t_chatElem	&elem = *it;
+      std::size_t	elapsed = elem.time.getElapsedTime().asMilliseconds();
 
+      if (elapsed > Chat::timeDelay)
+	elapsed = Chat::timeDelay;
+      if (elapsed > Chat::timeDelay / 2)
+	widget.setColor(sf::Color(0,0,0, 255 - ((elapsed - Chat::timeDelay / 2) * 255) /
+				  (Chat::timeDelay / 2)));
+      else
+	widget.setColor(sf::Color(0,0,0,255));
+      widget.setTextContent(elem.msg);
+      widget.move(0, -40);
       widget.alignText(sf::Vector2f(zone.left, zone.top),
 		       sf::Vector2f(zone.width, zone.height),
 		       50, 50);
