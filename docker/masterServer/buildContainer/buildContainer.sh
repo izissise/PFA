8beb959741e75850c6a6bb819451ca4243e15a0d
Hugues Morisset <morisset.hugues@gmail.com>
2015-02-26 02:09:18 +0100

    Correctly retrieve binary from build container

--- docker/masterServer/buildContainer/buildContainer.sh
+++ docker/masterServer/buildContainer/buildContainer.sh
@@ -4,4 +4,18 @@ rm -fr PFA
 mkdir -p PFA
 rsync -r ../../../ ./PFA/
 
-docker build -t "fusion_build" .
+CONT_NAME="fusion_build"
+CONT_ID="tmp_fusion_build"
+
+BUILD_PATH="/PFA/build/masterServer"
+DEST_PATH="`pwd`/../ressources/"
+
+docker build -t ${CONT_NAME} .
+
+echo "Build finished time to retrieve payload."
+docker run --name="${CONT_ID}" ${CONT_NAME}
+docker cp ${CONT_ID}:${BUILD_PATH} ${DEST_PATH}
+
+docker stop $CONT_ID
+docker rm $CONT_ID
+
